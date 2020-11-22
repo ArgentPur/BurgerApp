ca6c83e8df0bccf8c397ae00c216f28298d07725
Zachary Lynn <zalynn25@hotmail.com>
2020-11-22 16:27:47 -0500

    update

--- db/schema.sql
+++ db/schema.sql
@@ -3,5 +3,9 @@ create database burgers_db;
 use burgers_db;
 
 create table burgers {
+    id AUTO INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY id
     
 };
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+insert into burgers (name, devoured) values ("Burger1", false);
+insert into burgers (name, devoured) values ("Burger2", false);
+insert into burgers (name, devoured) values ("Burger3", false);
