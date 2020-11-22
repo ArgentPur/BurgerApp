2cb40fbe261aa8e303203f81fc9c7420e6f6fa30
Zachary Lynn <zalynn25@hotmail.com>
2020-11-22 12:46:21 -0500

    connectionjs

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("Connection Error: " + err.stack);
+        return;
+    }
+    console.log("Connected as ID: " + connection.threadId)
+});
+
+module.exports = connection;
