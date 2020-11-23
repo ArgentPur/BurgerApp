e07c844ce2df1ec0a4dcf3a8d0e2d7a63949a305
Zachary Lynn <zalynn25@hotmail.com>
2020-11-22 23:06:17 -0500

    update

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -13,6 +13,31 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post()
+router.post("/api/burgers", function(req, res) {
+    console.log(req.body);
+    burger.create([
+        "burger_name"
+    ],
+    [
+        req.body.name
+    ], function (result) {
+        res.json({id: result.insertId});
+    });
+});
 
-router.put()
+router.put("api/burgers/:id", function(req, res) {
+    var condition = `id = ${req.params.id}`;
+    console.log("condition", condition);
+    burger.update({
+        devoured: req.body.devour
+    }, condition, function(result) {
+        if(result.changedRows == 0) {
+            return res.status(404).end();
+        }else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+module.exports = router;
