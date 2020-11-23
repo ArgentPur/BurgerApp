c88930cbf9fe22c83aec2f9f2594c6fdd8085f20
Zachary Lynn <zalynn25@hotmail.com>
2020-11-22 21:15:34 -0500

    sqlFix

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,9 @@ create database burgers_db;
 
 use burgers_db;
 
-create table burgers {
-    id AUTO INCREMENT,
+create table burgers( 
+    id int AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN NOT NULL,
-    PRIMARY KEY id
-    
-};
+    PRIMARY KEY (id)
+    );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 use burgers_db;
 
-insert into burgers (name, devoured) values ("Burger1", false);
-insert into burgers (name, devoured) values ("Burger2", false);
-insert into burgers (name, devoured) values ("Burger3", false);
+insert into burgers (burger_name, devoured) values ("Burger1", false);
+insert into burgers (burger_name, devoured) values ("Burger2", false);
+insert into burgers (burger_name, devoured) values ("Burger3", false);
