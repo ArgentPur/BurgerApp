d3452fee85c2cba0b832942f9b7d9368d1d03407
Zachary Lynn <zalynn25@hotmail.com>
2020-11-22 21:32:26 -0500

    burger.js

---
+++ models/burger.js
@@ -0,0 +1,19 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+    all: function(cb) {
+        orm.all("burgers_db", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers_db", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers_db", objColsVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
