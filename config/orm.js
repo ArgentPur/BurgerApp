1707d64976dc055fe029fb39655b46886bcae322
Zachary Lynn <zalynn25@hotmail.com>
2020-12-21 23:43:22 -0500

    updateORM

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,16 @@
+var connection = require("./connection.js");
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryStr = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryStr, function(err, result) {
+            if(err){throw err;}cb(result)
+        });
+    }
+}
 selectAll()
 insertOne()
 updateOne()
+
+
+module.exports = orm;
