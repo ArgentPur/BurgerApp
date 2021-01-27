c0a6ebeeddd16d9b4ebf9d9b3dd63f0a2673af5e
Zachary Lynn <zalynn25@hotmail.com>
2021-01-26 23:30:01 -0500

    debugging

--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ const orm = require("../config/orm.js");
 
 const burger = {
     all: function(cb) {
-        orm.all("burgers_db", function(res) {
+        orm.all("burgers", function(res) {
             cb(res);
         });
     },
     create: function(cols, vals, cb) {
-        orm.create("burgers_db", cols, vals, function(res) {
+        orm.create("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
     update: function(objColVals, condition, cb) {
-        orm.update("burgers_db", objColsVals, condition, function(res) {
+        orm.update("burgers", objColsVals, condition, function(res) {
             cb(res);
         });
     }
